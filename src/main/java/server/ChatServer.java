7df6e055342e592c3e13eea88fe0f0de61c668a8
AndersLind99 <45364954+Razercake@users.noreply.github.com>
2021-03-05 12:25:07 +0100

    testing commits

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -7,7 +7,7 @@ import java.net.UnknownHostException;
 
 public class ChatServer {
 
-// first commit
+// testing new commits
     //Call server with arguments like this: 0.0.0.0 8088 logfile.log
     public static void main(String[] args) throws UnknownHostException {
         String ip ="localhost";
